41378144705c0675b2699bf5bd859ebad4078810
MahdiMohammadi98125 <109587937+MahdiMohammadi98125@users.noreply.github.com>
2022-09-14 11:20:32 -0700

    sticky navigation added

--- js/script.js
+++ js/script.js
@@ -37,6 +37,29 @@ allLinks.forEach(function (link) {
   });
 });
 
+// Sticky Navigation
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
